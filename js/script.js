65e54bd25a9ccd8dc3f8c2ea57557cf058510b5c
shuvogm <shuvogm@gmail.com>
2016-02-02 02:27:06 +0600

    ne3

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $("#footer").append('<div class="btn"><a href="#" target="_blank">Back To The Tutorial</a></div>');
+});
+
+
+$(window).load(function() {
+    var maxW = $('.flexslider').attr('style');
+    $('#code-section').attr('style', maxW);
+
+
+
+
+});
